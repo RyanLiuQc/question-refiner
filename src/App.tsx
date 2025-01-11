3404234be27ff271d6fd62cd5ac4c2149599dcd8
HANG_LIU <76828931+8HANGLIU8@users.noreply.github.com>
2025-01-11 04:05:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Layout from "./components/Layout";
-import Chat from "./pages/Chat";
-import FAQ from "./pages/FAQ";
-import Tips from "./pages/Tips";
-import Mentors from "./pages/Mentors";
-
 const queryClient = new QueryClient();
 
+
+
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Chat />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/tips" element={<Tips />} />
-            <Route path="/mentors" element={<Mentors />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+ <QueryClientProvider client={queryClient}>
+   <TooltipProvider>
+     <Toaster />
+     <Sonner />
+     <BrowserRouter>
+</BrowserRouter>
+   </TooltipProvider>
+ </QueryClientProvider>
 );
+
+
 
 export default App;
